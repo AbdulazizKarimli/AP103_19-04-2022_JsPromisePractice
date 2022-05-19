1c29da8259413c6842da030531beefcbb0c95494
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-05-19 14:09:04 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const numInp = document.querySelector("input[type='number']");
+const selectBox = document.querySelector(".form-select");
+const form = document.querySelector("form");
+const resultBox = document.getElementById("result");
+
+async function getCurrency(url) {
+  let promise = await fetch(url);
+  let response = await promise.json();
+
+  return response;
+}
+
+form.addEventListener("submit", change);
+
+async function change(e) {
+  e.preventDefault();
+
+  let response = await getCurrency(
+    "https://api.exchangerate.host/latest?base=AZN"
+  );
+
+  if (selectBox.value in response.rates && numInp.value >= 1) {
+    resultBox.innerText = numInp.value * response.rates[selectBox.value];
+  }
+}
